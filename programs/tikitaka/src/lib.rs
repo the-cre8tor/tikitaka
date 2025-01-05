2ed28cb75bb4ce65743f79ece0a39bfaa044b7f3
Alexander Nitiola <anitiola@aellacredit.com>
2025-01-06 00:28:25 +0100

    wrote the start instruction

--- programs/tikitaka/src/lib.rs
+++ programs/tikitaka/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use num_derive::*;
+use num_traits::*;
 
 declare_id!("H3g2QDsGtNtAcfuHDA6oeGdeq2w3LjxNrytS7FuwLFB5");
 
@@ -14,3 +16,54 @@ pub mod tikitaka {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Game {
+    players: [Pubkey; 2],
+    turn: u8,
+    board: [[Option<Sign>; 3]; 3],
+    state: GameState,
+}
+
+impl Game {
+    const PLAYERS_SIZE: usize = 32 * 2;
+    const TURN_SIZE: usize = 1;
+    const BOARD_SIZE: usize = 9 * (1 + 1);
+    const STATE_SIZE: usize = 33;
+
+    pub const MAXIMUM_SIZE: usize =
+        Self::PLAYERS_SIZE + Self::TURN_SIZE + Self::BOARD_SIZE + Self::STATE_SIZE;
+
+    pub fn start(&mut self, players: [Pubkey; 2]) -> Result<()> {
+        require_eq!(self.turn, 0, TikitakaError::GameAlreadyStarted);
+
+        self.players = players;
+        self.turn = 1;
+
+        Ok(())
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum GameState {
+    Active,
+    Tie,
+    Won { winner: Pubkey },
+}
+
+#[derive(
+    AnchorSerialize, AnchorDeserialize, FromPrimitive, ToPrimitive, Clone, Copy, PartialEq, Eq,
+)]
+pub enum Sign {
+    X,
+    O,
+}
+
+#[error_code]
+pub enum TikitakaError {
+    TileOutOfBounds,
+    TileAlreadySet,
+    GameAlreadyOver,
+    NotPlayersTurn,
+    GameAlreadyStarted,
+}
